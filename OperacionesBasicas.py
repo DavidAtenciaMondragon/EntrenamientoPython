55277679094c90176cb7b9d594c2cddbba4a95b9
David <davidatencia10@gmail.com>
2020-03-24 22:07:54 -0300

    Commit para que Raquel resuelva

--- OperacionesBasicas.py
+++ OperacionesBasicas.py
@@ -3,6 +3,7 @@
 
 a = 5
 b = -3
+c = 2
 
 suma     = a+b
 resta    = a-b
@@ -10,4 +11,26 @@ multi    = a*b
 divi     = a/b
 potencia = a**b
 
-print("branch de raquel")
+print("Para mostrar resultados se utiliza el comando print")
+print(suma)
+print(resta)
+print(multi)
+print(divi)
+print(potencia)
+
+# Ejercicios
+
+# 1) Convertir el valor de kilometros por hora a metros por segundo y mostrar el resultado
+
+metrosSegundo = 50
+# kilometrosHora = ?
+
+# 2) Sacar la raiz cuarta de 81 y mostrar el resultado
+
+num = 81
+# raiz4ta = ?
+
+# 3) Es lo mismo a + b * c que (a * b) + c? Probar
+
+# valor1 = ?
+# valor2 = ?
